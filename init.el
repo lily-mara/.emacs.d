4dd9df11f51f0446b21bf30430910d6c8d7d0902
Nate Mara <natemara@gmail.com>
2014-04-29 10:21:10 -0400

    initial commit

---
+++ init.el
@@ -0,0 +1,56 @@
+;;;; Evil mode
+;;;;;;;; Bootstrapper
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;;; from purcell/emacs.d
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(package-initialize)
+
+(require-package 'evil)
+
+;;;;;;;; Configuration
+(setq evil-search-module 'evil-search
+      evil-want-C-u-scroll t
+      evil-want-C-w-in-emacs-state t)
+
+(require 'evil)
+(evil-mode t)
+
+(define-key evil-insert-state-map "\C-c" 'evil-normal-state)
+(define-key evil-visual-state-map "\C-c" 'evil-normal-state)
+
+;;;;PACKAGES
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(global-git-gutter-mode +1)
+
+(load-theme 'monokai t)
+
+(require 'whitespace-mode)
+
+;;;;CONFIGS
+; set python to use tabs instead of spaces
+(add-hook 'python-mode-hook
+  (lambda ()
+    (setq indent-tabs-mode t)
+    (setq python-indent 8)
+    (setq tab-width 4)))
