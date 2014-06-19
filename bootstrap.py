d6cc9af647c64b244ba9e695851692053202a1d0
Nate Mara <natemara@gmail.com>
2014-06-19 15:44:49 -0400

    fixed pep8 errors

--- bootstrap.py
+++ bootstrap.py
@@ -8,22 +8,26 @@ plugins = [
 	'natemara/ahk-mode'
 ]
 
+
 def main():
 	clone_all_plugins()
+
 
 def full_url(url):
 	url_beginnings = ['http', 'git://', 'ssh://', 'https://']
 	fully_qualified = any((url.startswith(i) for i in url_beginnings))
+	
 	github = 'https://github.com'
-
+	
 	if fully_qualified:
 		return url
-	
+
 	if url.startswith('/'):
 		return github + url
 
 	return github + '/' + url
-		
+
+
 def clone_all_plugins():
 	home = os.path.expanduser('~')
 	plugins_dir = os.path.join(home, '.emacs.d', 'plugins')
