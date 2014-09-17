06229a5805234811655f27becf31654f8ad3c058
Nate Mara <natemara@gmail.com>
2014-09-16 22:43:50 -0400

    add code to use clang completion

--- init.el
+++ init.el
@@ -92,3 +92,23 @@
 (add-hook 'org-mode-hook (lambda () (evil-leader/set-key 
 				      "op" 'org-mobile-push
 				      "ou" 'org-mobile-pull)))
+
+(require 'auto-complete-clang-async)
+
+(defun ac-cc-mode-setup ()
+	(setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+	(setq ac-sources '(ac-source-clang-async))
+	(ac-clang-launch-completion-process)
+)
+
+(defun ac-common-setup ()
+	()
+)
+
+(defun my-ac-config ()
+	(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+	(add-hook 'auto-complete-mode-hook 'ac-common-setup)
+	(global-auto-complete-mode t)
+)
+
+(my-ac-config)
