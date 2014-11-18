20ec403a9ce8f50bb7a80a59080bc70285c7f722
Nate Mara <natemara@gmail.com>
2014-11-18 11:41:23 -0500

    fix whitespace in packages.el

--- packages.el
+++ packages.el
@@ -1,11 +1,8 @@
 ;;; Emacs is not a package manager, and here we load its package manager!
 (require 'package)
 (dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
-                  ("elpa" . "http://tromey.com/elpa/")
-                  ;; TODO: Maybe, use this after emacs24 is released
-                  ;; (development versions of packages)
-                  ("melpa" . "http://melpa.milkbox.net/packages/")
-                  ))
+  ("elpa" . "http://tromey.com/elpa/")
+  ("melpa" . "http://melpa.milkbox.net/packages/")))
   (add-to-list 'package-archives source t))
 (package-initialize)
 
@@ -18,9 +15,9 @@
   '(evil git-gutter monokai-theme magit markdown-mode evil-leader jedi evil-surround arduino-mode evil-nerd-commenter zeal-at-point haskell-mode auto-complete-clang-async editorconfig))
 
 (dolist (p tmtxt/packages)
-	  (when (not (package-installed-p p))
-		(package-install p))
-	  (require p))
+  (when (not (package-installed-p p))
+    (package-install p))
+  (require p))
 
 
 (require 'evil-org)
